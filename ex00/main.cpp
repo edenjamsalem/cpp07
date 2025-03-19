caa945c8eaa7c8bf79ff85ad2674ba6d72365dc5
Eden Judah Amsalem <eamsalem@c1r6s3.42london.com>
2025-03-19 15:23:55 +0000

    ex00: finished

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "whatever.hpp"
+#include <iostream> 
+
+int main()
+{
+	int a = 100;
+	int b = 101;
+
+	char c = 'c';
+	char d = 'd';
+	
+	std::cout << "(int) a: " << a << std::endl;
+	std::cout << "(int) b: " << b << std::endl;
+	std::cout << "(int) min: " << min(a, b) << std::endl;
+	std::cout << "(int) max: " << max(a, b) << std::endl;
+	swap(a, b);
+	std::cout << "(int) swap a: " << a << std::endl;
+	std::cout << "(int) swap b: " << b << std::endl << std::endl;
+
+	std::cout << "(char) c: '" << c << "'" << std::endl;
+	std::cout << "(char) d: '" << d << "'" << std::endl;
+	std::cout << "(char) min: " << min(c, d) << std::endl;
+	std::cout << "(char) max: " << max(c, d) << std::endl;
+	swap(c, d);
+	std::cout << "(char) swap c: " << a << std::endl;
+	std::cout << "(char) swap d: " << b << std::endl << std::endl;
+
+}
